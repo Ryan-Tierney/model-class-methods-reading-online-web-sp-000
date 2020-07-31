a8e842a3a6cbba56ce4884fdb6068970869eb25a
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-07-31 23:55:38 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,11 @@ class Post < ActiveRecord::Base
   end 
   
   def self.from_today
-    where
+    where("created_at >=?", Time.zone.today.beginning_of_day)
+  end 
+  
+  def self.old_news
+    whe
 
   private
 
